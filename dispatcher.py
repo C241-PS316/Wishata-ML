c980d0beb4d7c4a79ced032e2080f4430a1ee759
Muhammad Ridho K. Pratama <mharits.beyourself@gmail.com>
2024-05-21 11:29:43 +0700

    update file: dispatcher

--- dispatcher.py
+++ dispatcher.py
@@ -1,7 +1,8 @@
 import os
 import shutil
-from PIL import Image
-import matplotlib.pyplot as plt
+from PIL import Image, ImageTk
+import tkinter as tk
+from tkinter import simpledialog, messagebox
 
 def create_folders(main_folder, sub_folders):
     if not os.path.exists(main_folder):
@@ -15,11 +16,68 @@ def move_file(file_path, destination_folder):
     shutil.move(file_path, destination_folder)
     print("{} moved to {}".format(os.path.basename(file_path), destination_folder))
 
-def display_image(image_path):
-    img = Image.open(image_path)
-    plt.imshow(img)
-    plt.axis('off')
-    plt.show()
+class ImageMoverApp:
+    def __init__(self, root, main_folder, sub_folders):
+        self.root = root
+        self.main_folder = main_folder
+        self.sub_folders = sub_folders
+        self.shortcuts = {str(i+1): sub_folder for i, sub_folder in enumerate(sub_folders)}
+        self.undo_stack = []
+        self.downloads_folder = "downloads"
+        self.current_file = None
+        self.files = [f for f in os.listdir(self.downloads_folder) if os.path.isfile(os.path.join(self.downloads_folder, f))]
+
+        self.label = tk.Label(root)
+        self.label.pack()
+        
+        self.button_frame = tk.Frame(root)
+        self.button_frame.pack()
+        
+        self.create_buttons()
+
+        self.next_image()
+
+    def create_buttons(self):
+        for key, sub_folder in self.shortcuts.items():
+            button = tk.Button(self.button_frame, text=f"{key} - {sub_folder}", command=lambda k=key: self.move_image(k))
+            button.pack(side=tk.LEFT)
+        
+        undo_button = tk.Button(self.button_frame, text="Undo", command=self.undo_last_move)
+        undo_button.pack(side=tk.LEFT)
+        
+        skip_button = tk.Button(self.button_frame, text="Skip", command=self.skip_image)
+        skip_button.pack(side=tk.LEFT)
+
+    def next_image(self):
+        self.label.config(image='')
+        if self.files:
+            self.current_file = os.path.join(self.downloads_folder, self.files.pop(0))
+            img = Image.open(self.current_file)
+            img.thumbnail((400, 400))
+            self.img = ImageTk.PhotoImage(img)
+            self.label.config(image=self.img)
+        else:
+            messagebox.showinfo("Info", "No more images in the downloads folder.")
+            self.root.quit()
+
+    def skip_image(self):
+        self.next_image()
+
+    def move_image(self, shortcut):
+        if self.current_file and shortcut in self.shortcuts:
+            dest_folder = os.path.join(self.main_folder, self.shortcuts[shortcut])
+            move_file(self.current_file, dest_folder)
+            self.undo_stack.append((os.path.join(dest_folder, os.path.basename(self.current_file)), self.downloads_folder))
+            self.next_image()
+
+    def undo_last_move(self):
+        if self.undo_stack:
+            last_move = self.undo_stack.pop()
+            shutil.move(last_move[0], last_move[1])
+            self.files.insert(0, os.path.basename(last_move[0]))  # Reinsert file at the beginning of the list
+            self.next_image()
+        else:
+            print("Nothing to undo")
 
 def main():
     downloads_folder = "downloads"
@@ -29,7 +87,6 @@ def main():
     
     main_folder = input("Input nama main folder: ")
     sub_folders = []
-    shortcuts = {}
     i = 1
     
     while True:
@@ -37,39 +94,13 @@ def main():
         if sub_folder.lower() == 'q':
             break
         sub_folders.append(sub_folder)
-        shortcuts[str(i)] = sub_folder
         i += 1
     
     create_folders(main_folder, sub_folders)
     
-    undo_stack = []
-    
-    for filename in os.listdir(downloads_folder):
-        file_path = os.path.join(downloads_folder, filename)
-        if not os.path.isfile(file_path):
-            continue
-        
-        display_image(file_path)
-        print("Input shortcut number to move {} or 'p' to undo last move or 'l' to skip:".format(filename))
+    root = tk.Tk()
+    app = ImageMoverApp(root, main_folder, sub_folders)
+    root.mainloop()
 
-        while True:
-            choice = input().strip().lower()
-            
-            if choice in shortcuts:
-                dest_folder = os.path.join(main_folder, shortcuts[choice])
-                move_file(file_path, dest_folder)
-                undo_stack.append((os.path.join(dest_folder, filename), downloads_folder))
-                break
-            elif choice == 'p' and undo_stack:
-                last_move = undo_stack.pop()
-                shutil.move(last_move[0], last_move[1])
-                print("{} moved back to downloads folder".format(os.path.basename(last_move[0])))
-                break
-            elif choice == 'l':
-                print("Skipped {}".format(filename))
-                break
-            else:
-                print("Invalid choice. Try again.")
-    
 if __name__ == "__main__":
     main()
